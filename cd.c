d0df0cee1b4dc203e8717dd1f8679f4196d400c1
ekinnune <eljas.kinnunen98@gmail.com>
2023-06-28 16:56:05 +0300

    cd.c was missing??

--- cd.c
+++ cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: djames <djames@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ekinnune <ekinnune@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 15:44:35 by djames            #+#    #+#             */
-/*   Updated: 2023/05/24 17:11:26 by djames           ###   ########.fr       */
+/*   Updated: 2023/06/28 16:55:36 by ekinnune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,26 @@ int change_directory(char *path)
 	char buf[BUFFER];
     char **path1;
 
-    
-    if(path == NULL)
-    {
-        printf("esto es el home%s\n", g_data.envir[16]);
-        path1=ft_split(g_data.envir[16], '=');
-        printf("dividido%s\n", path1[1]);
-        path = path1[1];
-    }
+	if(path == NULL)
+	{
+		printf("esto es el home%s\n", g_data.envir[16]);
+		path1=ft_split(g_data.envir[16], '=');
+		printf("dividido%s\n", path1[1]);
+		path = path1[1];
+	}
 
-    if (chdir(path) == 0) {
-        printf("Directory changed successfully.\n");
+	if (chdir(path) == 0) {
+		printf("Directory changed successfully.\n");
 		result = getcwd(buf, sizeof(buf));
-        if (result != NULL) {
-            printf("Current working directory: %s\n", buf);
-        } else {
-            perror("MINISHELL");
-            return 1;
-        }
-    } else {
-        perror("MINISHELL");
-        return 1;
-    }
-    return 0;
+		if (result != NULL) {
+			printf("Current working directory: %s\n", buf);
+		} else {
+			perror("MINISHELL");
+			return 1;
+		}
+	} else {
+		perror("MINISHELL");
+		return 1;
+	}
+	return 0;
 }
