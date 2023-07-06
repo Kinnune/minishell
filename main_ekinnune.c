ae45c1b8cdcbdff7273d97949a88fdf1849b31cd
djames9 <108282283+djames9@users.noreply.github.com>
2023-07-06 14:53:51 +0300

    Add files via upload

--- main_ekinnune.c
+++ main_ekinnune.c
@@ -3,85 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   main_ekinnune.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ekinnune <ekinnune@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: djames <djames@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 11:10:42 by ekinnune          #+#    #+#             */
-/*   Updated: 2023/07/06 11:18:36 by ekinnune         ###   ########.fr       */
+/*   Updated: 2023/07/05 16:56:37 by djames           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+//check for unitialized values
+
+
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &(g_data.orig_termios));
+	//struct termios    term;
+  	tcgetattr(STDIN_FILENO, &(g_data.term));
+	g_data.term.c_lflag |= ECHOCTL;
+    tcsetattr(STDIN_FILENO, TCSANOW, &(g_data.term));
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &(g_data.orig_termios));
-  struct termios raw = g_data.orig_termios;
+	//struct termios    term;
+  tcgetattr(STDIN_FILENO, &(g_data.term));
+  struct termios raw = g_data.term;
   raw.c_lflag &= ~(ECHOCTL);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 void	handle_signal(int signal)
 {
-	enableRawMode();
+	//enableRawMode();
 	if(signal == SIGINT)
 	{
 		ioctl(STDOUT_FILENO, TIOCSTI, "\n");
 		rl_on_new_line();
 	}
 }
-
-//not sure if leaks fd's
-int	local_builtin(t_command *command)
+void	handle_signal2(int signal)
 {
-	int std_fd[3];
-	int ret_val;
-
-	ret_val = 1;
-	if (*command->redir)
-	{
-		std_fd[0] = dup(STDIN_FILENO);
-		std_fd[1] = dup(STDOUT_FILENO);
-		std_fd[2] = dup(STDERR_FILENO);
-	}
-	check_redirect_in(command->redir, command->here_doc);
-	check_redirect_out(command->redir);
-	ret_val = check_built(*command->cmd);
-	if (*command->redir)
-	{
-		dup2(std_fd[0], STDIN_FILENO);
-		dup2(std_fd[1], STDOUT_FILENO);
-		dup2(std_fd[2], STDERR_FILENO);
-	}
-	return (ret_val);
+	//disableRawMode();
+	if(signal == SIGINT)
+		write(1, "\n", 1);
 }
 
 int main(int argc, char **argv, char **envp)
 {
 	t_token *token;
 	t_command *command;
+	//char *input;
 	int i = argc;
 	char *new;
 	new = argv[1];
 	char *prom_line;
+	//int pid;
 	struct sigaction sa ;
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_flags =SA_RESTART;
 	sigemptyset(&sa.sa_mask);
 	sigaddset(&sa.sa_mask, SIGINT);
-
 	sigaction(SIGINT, &sa, NULL);
+	//pid = getpid();
 	copy_env(envp);
+	//printf("este es el pid %d\n", pid);
 	if(envp)
 		i++;
 	while (1)
 	{
-		i = 1;
 		command = NULL;
 		token = NULL;
-		
+		//input = readline(">"); // prom so here I will make the split
+		enableRawMode();
 		signal(SIGQUIT, SIG_IGN);
 		signal(SIGINT, handle_signal);
 		prom_line = readline("MINISHELL$ ");
@@ -95,24 +86,116 @@ int main(int argc, char **argv, char **envp)
 			exit(0);
 		} 
 		ft_history(prom_line);
-
+		// i = check_built(prom_line);// it is need to check in the pipe 
+		// if(i == 256)
+		// {
+		// 	printf("exit\n");//writeestandar erro print dprintf not allow 
+		// 	printf("MINISHELL: exit: a: numeric argument required\n");
+		// 	i = 255;
+		// }
+		// else if(i >= 0 && i <= 255)
+		// 	printf("exit\n");
+		// else if(i == 256)
+		// {
+		// 	printf("exit\n");
+		// 	printf("MINISHELL: exit: too many arguments\n");
+		// 	i = 255;
+		// }
+		i=257;
+		if(i == argc || i == 257)
+	{
 		
-		if (!count_quotes(prom_line))
+		if (!count_quotes(prom_line)) // cahnce to prom_line
 			token = tokenizer(prom_line);
 		if (!check_tokens(token))
 		{
+			// print_tokens(token);
 			command = convert_tokens(token);
-			expand_command_args(command);
-			if (!command->next)
-				i = local_builtin(command);
-			if (i)
-				check_list(command);
-			free_tokens(token);
+			i =check_list(command);
+			if(i == 258)
+			{
+				printf("nyt on toimi \n");
+				i = check_built(prom_line);// it is need to check in the pipe 
+				if(i == 256)
+				{
+					printf("exit\n");//writeestandar erro print dprintf not allow 
+					printf("MINISHELL: exit: a: numeric argument required\n");
+					i = 255;
+				}
+				else if(i >= 0 && i <= 255)
+					printf("exit\n");
+				else if(i == 256)
+				{
+					printf("exit\n");
+					printf("MINISHELL: exit: too many arguments\n");
+					i = 255;
+				}
+			}
+			// build_pipes(command);
+			//handle_commands(command);
+			//print_commands(command);// this we will not need
+			// free_tokens(token);
+			// printf("freed tokens\n");
 			token = NULL;
 		}
-		free_commands(command);
-		if(prom_line)
-			free(prom_line);
+		// free_commands(command);
+		// printf("freed commands\n");
+        // execve(get_path(*command->cmd),command->cmd, g_data.envir);
+		//if (input)
+		//	free(input);
 	}
+		if(prom_line)
+		{
+			free(prom_line);
+		}
+		// if(i != 257)
+		// 	exit(i);
+	
+	
+	}
+	//printf("%d", i);
+	disableRawMode();
 	return (i);
+	//return (0);
 }
+
+// void	build_pipes(t_command *command)
+// {
+// 	if (!command)
+// 		return ;
+// 	// handle_pipes(command);
+// 	build_pipes(command->next);
+// }
+
+// void	build_redirections(t_command *command)
+// {
+// 	if (!command)
+// 		return ;
+// 	if (!*(command->redir))
+// 		return ;
+// 	handle_redirections(command);
+// 	// if (command->next)
+// 	// 	build_redirecitons(command->next);
+// }
+
+// // command->next
+// // *command->redir;
+// // execve(get_path(*command->cmd), (command->cmd + 1), g_data.envir);
+
+// void	handle_commands(t_command *command)
+// {
+// 	pid_t pid;
+
+// 	if (!command)
+// 		return ;
+// 	pid = fork();
+// 	if (pid < 0)
+// 		return ;
+// 	if (pid == 0)
+// 	{
+// 		build_redirections(command);
+// 		// run_command(command);
+// 	}
+// 	else
+// 		handle_commands(command->next);
+// }
--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ekinnune <ekinnune@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: djames <djames@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 16:10:29 by djames            #+#    #+#             */
-/*   Updated: 2023/07/06 10:08:24 by ekinnune         ###   ########.fr       */
+/*   Updated: 2023/07/05 16:55:57 by djames           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ typedef struct s_data
 {
 	char **envir;
 	int flag;
-	struct termios orig_termios;
+	struct termios term;
 }			t_data;
 
 t_data	g_data;
@@ -65,12 +65,14 @@ void add_quotes(char* input) ;
 int ft_length_word(char **envp);
 char *find_equal_2(char *str);
 void remove_start(char **str);
+void disableRawMode();
 
 int ft_exec(t_command *command, int i, int **fd, pid_t *pid);
 int piepe_function(t_command *list, int i);
 int ft_free(int **fd, int *pid, int i);
 int close_pipe(int **fd, int i);
-void	check_list(t_command *command);
+int	check_list(t_command *command);
+void	handle_signal2(int signal);
 
 
 //------
@@ -106,25 +108,15 @@ int check_tokens(t_token *token);
 char	*here_doc(char *key);
 int	syntax_error(void);
 
-//expand_var.c
-int	valid_varname(char c);
-int	expand_command_args(t_command *command);
-char	*expand_var(char *str);
-char *check_var_logic(char **str);
-int check_dollar(char *str);
-
-
 //paths.c
 void	print_environ(void);
 char	*dot_slash_remove(char *path);
 char	*get_path(char *command);
 
 //redir.c
-int	redirect_out(int append, char *filename);
-int	redirect_in(int delimiter, char *filename);
-int	check_redirect(t_command *command, int new_fd);
-int	check_redirect_out(char **redir);
-int	check_redirect_in(char **redir, char *here_doc);
+int			redirect_out(int append, char *filename);
+int			redirect_in(int delimiter, char *filename);
+int			check_redirect(t_command *command, int new_fd);
 
 //tokensis.c
 t_token	*tokenizer(const char *input);
@@ -154,7 +146,6 @@ t_token	*skip_pipe(t_token *token);
 t_command	*convert_tokens(t_token *token);
 void print_commands(t_command *command);
 void	free_commands(t_command *command);
-void	free_2d(char **array);
 
 //------
 #endif
